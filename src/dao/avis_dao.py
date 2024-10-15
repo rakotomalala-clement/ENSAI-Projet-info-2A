ae68870990a4a4f700257d9c58796ed368804680
Lilian97116 <l.largitte@outlook.fr>
2024-10-15 22:13:27 +0200

    modification class dao_avis

--- src/dao/avis_dao.py
+++ src/dao/avis_dao.py
@@ -15,110 +15,164 @@ class DaoAvis(metaclass=Singleton):
     de gérer les avis
     """
 
-    def __init__(self, id_utilisateur, id_manga, avis_manga: Avis):
+    @log
+    def creer_avis(self, id_utilisateur, id_manga, avis_manga: Avis):
+        """Création d'un avis sur un manga dans la base de donnée
 
-        @log
-        def creer_avis(self, id_utilisateur, id_manga):
-            """Création d'un avis sur un manga dans la base de donnée
+        Parameters:
+        -----------
 
-            Parameters:
-            -----------
+        id_utilisateur: int
+            identifiant de l'utilisateur pour lequel on souhaite créer
+            un avis
 
-            id_utilisateur: int
-                identifiant de l'utilisateur pour lequel on souhaite créer
-                un avis
+        id_manga: int
+        identifiant du manga sur lequel l'utilisateur souhaite laisser un
+        avis
 
-            id_manga: int
-            identifiant du manga sur lequel l'utilisateur souhaite laisser un
-            avis
+        Returns:
+        --------
 
-            Returns:
-            --------
+        """
 
-            """
+        res = None
 
-            res = None
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "INSERT INTO "
+                        "avis(id_utilisateur, id_jikan_manga, avis, note) "
+                        "VALUES "
+                        "(%(id_utilisateur)s,%(id_jikan_manga)s,%(avis)s, "
+                        "%(note)s) "
+                        "RETURNING id_jikan_manga; ",
+                        {
+                            "id_utilisateur": id_utilisateur,
+                            "id_jikan_manga": id_manga,
+                            "avis": avis_manga.avis,
+                            "note": avis_manga.note,
+                        },
+                    )
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
 
-            try:
-                with DBConnection().connection as connection:
-                    with connection.cursor() as cursor:
-                        cursor.execute(
-                            "INSERT INTO "
-                            "avis(id_utilisateur, id_jikan_manga, avis, note) "
-                            "VALUES "
-                            "(%(id_utilisateur)s,%(id_jikan_manga)s,%(avis)s, "
-                            "%(note)s) "
-                            "RETURNING id_avis; ",
-                            {
-                                "id_utilisateur": id_utilisateur,
-                                "id_jikan_manga": id_manga,
-                                "avis": avis_manga.avis,
-                                "note": avis_manga.note,
-                            },
-                        )
-                        res = cursor.fetchone()
-            except Exception as e:
-                logging.info(e)
+        created = False
+        if res:
+            avis_manga.id_avis = res["id_avis"]
+            created = True
 
-            created = False
-            if res:
-                avis_manga.id_avis = res["id_avis"]
-                created = True
+        return created
 
-            return created
+    @log
+    def creer_avis_collection_coherente(
+        self, id_utilisateur, id_collection, avis_collection_coherente: Avis
+    ):
+        """Création d'un avis sur une collection cohérente dans la base
+        de données
 
-        @log
-        def creer_avis_collection_coherente(
-            self, id_utilisateur, id_collection, avis_collection_coherente: Avis = None
-        ):
-            """Création d'un avis sur une collection cohérente dans la base
-            de données
+        Parameters:
+        -----------
 
-            Parameters:
-            -----------
+        id_utilisateur: int
+            identifiant de l'utilisateur pour lequel on souhaite créer
+            un avis
 
-            id_utilisateur: int
-                identifiant de l'utilisateur pour lequel on souhaite créer
-                un avis
+        id_collection: int
+        identifiant de la collection sur lequel l'utilisateur souhaite
+        laisser un avis
 
-            id_collection: int
-            identifiant de la collection sur lequel l'utilisateur souhaite
-            laisser un avis
+        Returns:
+        --------
 
-            Returns:
-            --------
+        """
 
-            """
+        res = None
 
-            res = None
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "INSERT INTO "
+                        "avis_collection_coherente("
+                        "id_utilisateur, id_collection_coherente, avis"
+                        ",note) "
+                        "VALUES "
+                        "(%(id_utilisateur)s,%(id_collection_coherente)s,"
+                        "%(avis)s, "
+                        "%(note)s) "
+                        "RETURNING id_avis; ",
+                        {
+                            "id_utilisateur": id_utilisateur,
+                            "id_collection_coherente": id_collection,
+                            "avis": avis_collection_coherente.avis,
+                            "note": avis_collection_coherente.note,
+                        },
+                    )
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
 
-            try:
-                with DBConnection().connection as connection:
-                    with connection.cursor() as cursor:
-                        cursor.execute(
-                            "INSERT INTO "
-                            "avis_collection_coherente("
-                            "id_utilisateur, id_collection_coherente, avis"
-                            ",note) "
-                            "VALUES "
-                            "(%(id_utilisateur)s,%(id_jikan_manga)s,"
-                            "%(avis)s, "
-                            "%(note)s) "
-                            "RETURNING id_avis; ",
-                            {
-                                "id_utilisateur": id_utilisateur,
-                                "id_collection_coherente": id_collection,
-                                "avis": avis_collection_coherente.avis,
-                                "note": avis_collection_coherente.note,
-                            },
-                        )
-                        res = cursor.fetchone()
-            except Exception as e:
-                logging.info(e)
+        created = False
+        if res:
+            avis_collection_coherente.id_avis = res["id_avis_collection_coherente"]
+            created = True
 
-            created = False
-            if res:
-                avis_collection_coherente.id_avis = res["id_avis_collection_coherente"]
-                created = True
+        return created
 
-            return created
+    @log
+    def creer_avis_collection_physique(
+        self, id_utilisateur, id_collection, avis_collection_physique: Avis
+    ):
+        """Création d'un avis sur une collection physique dans la base
+        de données
+
+        Parameters:
+        -----------
+
+        id_utilisateur: int
+            identifiant de l'utilisateur pour lequel on souhaite créer
+            un avis
+
+        id_collection: int
+        identifiant de la collection sur lequel l'utilisateur souhaite
+        laisser un avis
+
+        Returns:
+        --------
+
+        """
+
+        res = None
+
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "INSERT INTO "
+                        "avis_collection_physique("
+                        "id_utilisateur, id_collection_physique, avis"
+                        ",note) "
+                        "VALUES "
+                        "(%(id_utilisateur)s,%(id_collection_physique)s,"
+                        "%(avis)s, "
+                        "%(note)s) "
+                        "RETURNING id_avis; ",
+                        {
+                            "id_utilisateur": id_utilisateur,
+                            "id_collection_physique": id_collection,
+                            "avis": avis_collection_physique.avis,
+                            "note": avis_collection_physique.note,
+                        },
+                    )
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
+
+        created = False
+        if res:
+            avis_collection_physique.id_avis = res["id_avis_ccollection_physique"]
+            created = True
+
+        return created
