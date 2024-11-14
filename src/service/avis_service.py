3e574bff0714e312e5806cd5b9880f80452551b0
Lilian97116 <l.largitte@outlook.fr>
2024-11-14 14:14:02 +0100

    ajustement fonc moififier avis dans ervice avis

--- src/service/avis_service.py
+++ src/service/avis_service.py
@@ -44,34 +44,39 @@ class ServiceAvis:
             raise Exception("type_collection incorrect")
 
     @log
-    def afficher_avis_user(self, id_utlisateur, id_manga):
+    def afficher_avis_user(id_utlisateur, id_manga):
         """Afficher l'avis qu'un utilisateur a laisser sur un  manga"""
 
-        avis_user_sur_manga = DaoAvis().chercher_avis("projet_info_2a", id_utlisateur, id_manga)
+        avis_user_sur_manga = DaoAvis.chercher_avis(id_utlisateur, id_manga)
 
         if len(avis_user_sur_manga) > 0:
             return avis_user_sur_manga[0]
         else:
             return None
 
-
     @log
-    def modifier(self, avis_message, note) -> Avis:
+    def modifier(self, id_manga, id_utilisateur, avis_message, note) -> Avis:
         """Modification d'un avis"""
         avis = Avis(note, avis_message)
 
-        return avis if DaoAvis().modifier_avis(schema="projet_info_2a", id_manga=, id_utilisateur=, avis=avis) else None
+        return (
+            avis
+            if DaoAvis().modifier_avis(
+                schema="projet_info_2a", id_manga=id_manga, id_utilisateur=id_utilisateur, avis=avis
+            )
+            else None
+        )
 
     @log
     def supprimer(self, id_avis) -> bool:
         """Supprimer un avis"""
-        return DaoAvis().supprimer_avis("projet_info_2a", id_avis)
+        return DaoAvis().supprimer_avis(id_avis)
 
     @log
     def afficher_autre_avis(self, id_manga):
         """Afficher les avis laisser sous  un manga"""
 
-        avis_user_sur_manga = DaoAvis().chercher_avis_sur_manga("projet_info_2a", id_manga)
+        avis_user_sur_manga = DaoAvis().chercher_avis_sur_manga("projet_test_dao", id_manga)
 
         if len(avis_user_sur_manga) > 0:
             return avis_user_sur_manga[0]
