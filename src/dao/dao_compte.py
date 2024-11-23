04acdecdff215f688f9e0e52260cd1b1f475f4fd
Imane-code-ux <imane.baladi@um6p.ma>
2024-11-23 15:31:17 +0100

    ok

--- src/dao/dao_compte.py
+++ src/dao/dao_compte.py
@@ -6,11 +6,14 @@ from dao.db_connection import DBConnection
 from utils.log_decorator import log
 import logging
 import re
-from dao.db_connection import DBConnection
-import os
 
 
 class DaoCompte(metaclass=Singleton):
+    """
+    La classe DaoCompte gère les opérations liées aux utilisateurs dans la base de données.
+    Elle permet de créer, lire, mettre à jour, supprimer et lister les utilisateurs, ainsi que de valider les mots de passe,et
+    elle utilise un modèle Singleton pour garantir qu'il n'y a qu'une seule instance de la classe dans l'application.
+    """
 
     def _valider_mot_de_passe(self, mot_de_passe: str) -> bool:
         """Vérifie la validité du mot de passe selon des critères de sécurité."""
@@ -33,6 +36,9 @@ class DaoCompte(metaclass=Singleton):
         return True
 
     def creer_utilisateur(self, utilisateur: Utilisateur) -> bool:
+        """
+        Crée un nouvel utilisateur dans la base de données avec les informations fournies dans l'objet Utilisateur.
+        """
         try:
             with DBConnection().connection as connection:
                 with connection.cursor() as cursor:
@@ -55,7 +61,9 @@ class DaoCompte(metaclass=Singleton):
             raise
 
     def trouver_utilisateur_par_id(self, id_utilisateur: int) -> Utilisateur:
-
+        """
+        Recherche un utilisateur par son identifiant et retourne un objet Utilisateur correspondant.
+        """
         select_query = """
         SELECT id_utilisateur, nom_utilisateur, mdp FROM utilisateur WHERE id_utilisateur = %(id_utilisateur)s;
         """
@@ -77,7 +85,9 @@ class DaoCompte(metaclass=Singleton):
 
     @log
     def trouver_utilisateur_par_nom(self, nom_utilisateur: str) -> Utilisateur:
-
+        """
+        Recherche un utilisateur par son nom d'utilisateur et retourne un objet Utilisateur correspondant.
+        """
         select_query = """
         SELECT id_utilisateur, nom_utilisateur, mdp FROM utilisateur WHERE nom_utilisateur = %(nom_utilisateur)s;
         """
@@ -101,7 +111,9 @@ class DaoCompte(metaclass=Singleton):
     def mettre_a_jour_utilisateur(
         self, id_utilisateur: int, nom_utilisateur: str, mdp: str
     ) -> bool:
-
+        """
+        Met à jour les informations d'un utilisateur (nom et mot de passe) dans la base de données.
+        """
         update_query = """
         UPDATE utilisateur
         SET nom_utilisateur = %(nom_utilisateur)s, mdp = %(mdp)s
@@ -136,6 +148,9 @@ class DaoCompte(metaclass=Singleton):
         return True
 
     def lister_tous(self) -> list[Utilisateur]:
+        """
+        Récupère et retourne tous les utilisateurs de la base de données sous forme de liste d'objets Utilisateur.
+        """
 
         try:
             with DBConnection("projet_info_2a").connection as connection:
